670a6bfc4c551419a539028d8dd80b92ef1f6758
DESKTOP-IAIBR9Q\Rahumath Marini <marinirahumath@gmail.com>
2017-06-30 09:12:37 +0800

    Modify Address, Email, Phone classes to follow inheritence (exends from Contact class)

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email extends Contact{
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
@@ -14,7 +14,6 @@ public class Email {
     public static final String EMAIL_VALIDATION_REGEX = "[\\w\\.]+@[\\w\\.]+";
 
     public final String value;
-    private boolean isPrivate;
 
     /**
      * Validates given email.
@@ -55,7 +54,5 @@ public class Email {
     }
 
 
-    public boolean isPrivate() {
-        return isPrivate;
-    }
+
 }
